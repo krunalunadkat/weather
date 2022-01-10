a2f3f0e4746f0ec3699c502f1dbb13b10fa42782
krunal <krunalunadkat2105@gmail.com>
2022-01-10 09:47:02 +0530

    Updated twig  & css

---
+++ web/sites/default/files/php/twig/61dbb236c3948_node--weather.html.twig_fb5hDkSMcoLbWESXtE6Xb73d6/w3g8IFuVNKhMDeYrLsGNIyY0fiW7tNAreqems1ZeN2w.php
@@ -0,0 +1,131 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/weather/templates/content/node--weather.html.twig */
+class __TwigTemplate_e9d72b6322364298912eae038f7c444f47501063ef0924a70d327d3985b822b7 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 62
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->attachLibrary("bootstrap_barrio/node"), "html", null, true);
+        echo "
+
+";
+        // line 65
+        $context["classes"] = [0 => "node", 1 => ("node--type-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source,         // line 67
+($context["node"] ?? null), "bundle", [], "any", false, false, true, 67), 67, $this->source))), 2 => ((twig_get_attribute($this->env, $this->source,         // line 68
+($context["node"] ?? null), "isPromoted", [], "method", false, false, true, 68)) ? ("node--promoted") : ("")), 3 => ((twig_get_attribute($this->env, $this->source,         // line 69
+($context["node"] ?? null), "isSticky", [], "method", false, false, true, 69)) ? ("node--sticky") : ("")), 4 => (( !twig_get_attribute($this->env, $this->source,         // line 70
+($context["node"] ?? null), "isPublished", [], "method", false, false, true, 70)) ? ("node--unpublished") : ("")), 5 => ((        // line 71
+($context["view_mode"] ?? null)) ? (("node--view-mode-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(($context["view_mode"] ?? null), 71, $this->source)))) : ("")), 6 => "clearfix"];
+        // line 75
+        echo "<article";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method", false, false, true, 75), 75, $this->source), "html", null, true);
+        echo ">
+  <div";
+        // line 76
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content_attributes"] ?? null), "addClass", [0 => "node__content", 1 => "clearfix"], "method", false, false, true, 76), 76, $this->source), "html", null, true);
+        echo ">
+    <div class=\"sticky-section\">
+      <div class=\"weather-node-title\">
+        <h3>";
+        // line 79
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["node"] ?? null), "label", [], "any", false, false, true, 79), 79, $this->source), "html", null, true);
+        echo "</h3>
+      </div>
+      <div class=\"weather-section\">
+        <div class=\"weather-card\">
+          <h3>";
+        // line 83
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["current_temperature"] ?? null), 83, $this->source), "html", null, true);
+        echo "</h3>
+        </div>
+      </div>
+    </div>
+    ";
+        // line 87
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content"] ?? null), 87, $this->source), "html", null, true);
+        echo "
+
+  </div>
+</article>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/weather/templates/content/node--weather.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  76 => 87,  69 => 83,  62 => 79,  56 => 76,  51 => 75,  49 => 71,  48 => 70,  47 => 69,  46 => 68,  45 => 67,  44 => 65,  39 => 62,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/custom/weather/templates/content/node--weather.html.twig", "D:\\install\\xampp\\htdocs\\weather\\web\\themes\\custom\\weather\\templates\\content\\node--weather.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 65);
+        static $filters = array("escape" => 62, "clean_class" => 67);
+        static $functions = array("attach_library" => 62);
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set'],
+                ['escape', 'clean_class'],
+                ['attach_library']
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
